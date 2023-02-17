57085bf4bdc4ecb1fc67becf900a6894f3c3d6cc
Albin Wahlén <alegow8@gmail.com>
2023-02-17 10:21:23 +0100

    Prepare for threads

--- src/verifier.cpp
+++ src/verifier.cpp
@@ -6,24 +6,35 @@
 
 using namespace std;
 
-Verifier::Verifier(PetriNet const &petri_net): constraints{petri_net.get_parser()}, initial_net{petri_net} {}
+Verifier::Verifier(PetriNet const &petri_net):
+    constraints{petri_net.get_parser()}, initial_net{petri_net} {}
+
+Verifier::~Verifier() {}
 
 void Verifier::verify() {
     previous_states.clear();
+    abort = false;
     token_max = initial_net.get_state().tokens;
     verify(initial_net, 0);
+    // Todo wait for all threads
+    if (abort) {
+        rethrow_exception(exception);
+    }
 }
 
 void Verifier::verify(PetriNet const &petri_net, unsigned current_depth) {
 
     if (current_depth > constraints.max_depth()) {
-        throw runtime_error{string("Verifier reached max depth (") + to_string(constraints.max_depth()) + string(")")};
+        abort = true;
+        string msg{
+            string("Verifier reached max depth (") +
+            to_string(constraints.max_depth()) + string(")")
+        };
+        exception = make_exception_ptr(runtime_error{msg});
     }
 
     check_boundness(petri_net);
     check_reachability(petri_net);
-
-    previous_states.insert(petri_net.get_state());
 
     unsigned const transition_count = petri_net.get_transition_count();
     vector<bool> fire_vector(transition_count, false);
@@ -49,9 +60,10 @@ void Verifier::verify(PetriNet const &petri_net, unsigned current_depth) {
             ++live_transitions;
             // Legal fire, active_net is now in a new state
             PetriNetState const &state = active_net.get_state();
-            if (previous_states.count(state) == 0) {
+            if (!reached_state(state)) {
                 // This is a new state
-                verify(active_net, current_depth + 1);
+                if (!abort)
+                    verify(active_net, current_depth + 1);
             } else {
                 // Known state
             }
@@ -68,16 +80,12 @@ void Verifier::verify(PetriNet const &petri_net, unsigned current_depth) {
         }
     }
 
-    if (live_transitions == 0) {
-        if (constraints.require_live()) {
-            throw LivenessException{
-                "PetriNet not live at depth " + to_string(current_depth) + 
-                "\n\tState: " + petri_net.str()};
-        }
-    }
+    check_liveness(petri_net, live_transitions, current_depth);
+
 }
 
 void Verifier::check_boundness(PetriNet const &net) {
+    std::unique_lock lock(mtx);
     PetriNetState const &current_state = net.get_state();
     bool constraints_satisfied = true;
     for (unsigned i = 0; i < initial_net.get_place_count(); ++i) {
@@ -104,17 +112,36 @@ void Verifier::check_boundness(PetriNet const &net) {
         ss << place_names[place_names.size() - 1] << ": "
            << current_state.tokens[place_names.size() - 1] << "/"
            << constraints.get_max_token(place_names.size() - 1);
-
-        throw BoundednessException("Net does not satisfy boundedness constraints: " + ss.str());
+        exception = make_exception_ptr(BoundednessException("Net does not satisfy boundedness constraints: " + ss.str()));
+        abort = true;
     }
 }
 
 void Verifier::check_reachability(PetriNet const &net) {
     if (constraints.is_illegal(net.get_state())) {
+        abort = true;
+        std::unique_lock lock(mtx);
         stringstream ss;
         for (auto token : net.get_state().tokens)
             ss << token << " ";
-        throw ReachabilityException{"Illegal state (" + ss.str() + ") reached"};
+        exception = make_exception_ptr(
+            ReachabilityException{"Illegal state (" + ss.str() + ") reached"}
+        );
+    }
+}
+
+void Verifier::check_liveness(PetriNet const &petri_net, unsigned live_transitions, unsigned current_depth) {
+    if (live_transitions == 0) {
+        if (constraints.require_live()) {
+            abort = true;
+            std::unique_lock lock(mtx);
+            exception = make_exception_ptr(
+                    LivenessException{
+                        "PetriNet not live at depth " + to_string(current_depth) +
+                        "\n\tState: " + petri_net.str()
+                    }
+            );
+        }
     }
 }
 
@@ -127,8 +154,14 @@ string Verifier::get_max_bounds_as_string() const {
     return "";
 }
 
-bool Verifier::reached_state(PetriNetState const &state) const {
-    return previous_states.count(state);
+bool Verifier::reached_state(PetriNetState const &state) {
+    std::unique_lock lock(mtx);
+    if (previous_states.count(state) > 0) {
+        return true;
+    } else {
+        previous_states.insert(state);
+        return false;
+    }
 }
 
 size_t Verifier::get_state_count() const {
--- src/verifier.h
+++ src/verifier.h
@@ -4,19 +4,30 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
 
 #include "petri_net.h"
 #include "verification_exceptions.h"
 #include "constraints.h"
+//#include "thread_pool.h"
 
 class Verifier {
 public:
     Verifier(PetriNet const &petri_net);
+    ~Verifier();
 
     void verify();
     std::vector<unsigned> const& get_max_bounds() const;
     std::string get_max_bounds_as_string() const;
-    bool reached_state(PetriNetState const &state) const;
+
+    /* Chech whether a state has been reached before.
+     * If not add it to previous_states
+     *
+     * Thread safe. */
+    bool reached_state(PetriNetState const &state) ;
+
     size_t get_state_count() const;
 
     Constraints constraints;
@@ -24,10 +35,18 @@ private:
     void verify(PetriNet const &initial_net, unsigned current_depth);
     void check_boundness(PetriNet const &net);
     void check_reachability(PetriNet const &net);
+    void check_liveness(PetriNet const &net, unsigned live_transactions, unsigned current_depth);
 
     std::unordered_set<PetriNetState, PetriNetStateHash> previous_states{};
     PetriNet const initial_net;
     std::vector<unsigned> token_max{};
+
+    //ThreadPool thread_pool;
+    std::mutex mtx{};
+    std::condition_variable cv{};
+
+    std::atomic<bool> abort = false;
+    std::exception_ptr exception = nullptr;
 };
 
 #endif  // VERIFIER_H_
